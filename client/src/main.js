87f297ade0c05187ea1696bc4b953dd14925f69b
SHIGETA <faliplvsg@gmal.com>
2019-07-22 08:55:00 +0900

    コンポーネントmixin実装

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import ui from './ui';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  ui,
   render: h => h(App),
 }).$mount('#app');
---
+++ client/src/ui.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+/** component as page */
+import HelloWorld from './components/HelloWorld.vue';
+
+/* common ui components */
+
+Vue.mixin({
+  components: {
+    HelloWorld,
+  },
+});
